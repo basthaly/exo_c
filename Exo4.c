f26e5c22a7a53568bcf0abf0aff6b3ec323ac0f7
Basthaly <bastien48@live.fr>
2020-02-05 11:38:59 +0100

    ajout exo4-5

---
+++ Exo4.c
@@ -0,0 +1,27 @@
+#include "toolbox.h"
+
+int main(int argc, char *argv[]){
+    unsigned char message [100];
+    unsigned char a;
+    unsigned char b;
+    
+    message[0]= 'h' ;
+    message[1]= 'e' ;
+    message[2]= 'l' ;
+    message[3]= 'l' ;
+    message[4]= 'o' ;
+    message[5]= ' ' ;
+    message[6]= 'w' ;
+    message[7]= 'o' ;
+    message[8]= 'r' ;
+    message[9]= 'l' ;
+    message[10]= 'd' ;
+    message[11]= ' ' ;
+    message[12]= ':' ;
+    message[13]= ')' ;
+    message[14]= '\0' ;
+
+    printf("%s\n",message);
+    min_maj(message);
+    printf("%s\n",message);
+}
