36bcbaacde3607cca029a7cb759b58632d5a1759
Basthaly <bastien48@live.fr>
2020-02-05 11:38:47 +0100

    ajout exo4-5

--- toolbox.c
+++ toolbox.c
@@ -16,3 +16,33 @@ void repl(char * var, unsigned char a, unsigned char b){
             var[i]=b;
     }
 }
+
+void min_maj(char * var){
+    int res;
+    int i;
+
+    res=compt(var);
+    for (i=0; i<res; i++){
+        if (var[i] >= 97 && var[i] <= 122)
+            var[i]=var[i]-32;
+    }
+}
+
+void mirror(char * var){
+    int res;
+    int i;
+    int a;
+
+    res=compt(var);
+    char tab[res];
+    a=0;
+
+    for (i=res-1; i>=0; i--){
+        tab[a]=var[i];
+        a++;
+    }
+
+    for (i=0; i<res; i++){
+        var[i]=tab[i];
+    }
+}
--- toolbox.h
+++ toolbox.h
@@ -1,3 +1,7 @@
 int compt (char);
 
 void repl(char *, unsigned char, unsigned char);
+
+void min_maj(char *);
+
+void mirror(char *);
